a5884c7e28a48dcdad24aeed4d5d4babffc745fd
YukiTenko <54477524+YukiTenko@users.noreply.github.com>
2023-05-24 15:35:15 +0700

    Add files via upload

--- test2.c
+++ test2.c
@@ -1,53 +1,62 @@
-// ftok(): is use to generate a unique key.
-// msgget(): either returns the message queue identifier for a newly created message queue or returns the identifiers for a queue which exists with the same key value.
-// msgsnd(): Data is placed on to a message queue by calling msgsnd().
-// msgrcv(): messages are retrieved from a queue.
-// msgctl(): It performs various operations on a queue. Generally it is use to destroy message queue.
-
 #include <stdio.h>
 #include <stdlib.h>
-#include <errno.h>
+#include <string.h>
+#include <time.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/stat.h>
 #include <sys/types.h>
-#include <sys/ipc.h>
-#include <sys/msg.h>
+#include <errno.h>
+#include <mqueue.h>
 
-#define PERMS 0644
-struct my_msgbuf {
-   long mtype;
-   char mtext[200];
-};
+#define QUEUE_NAME  "/test1totest2" /* Queue name. */
+#define QUEUE_PERMS 0644
+#define QUEUE_MAXMSG  5 /* Maximum number of messages. */
+#define QUEUE_MSGSIZE 99999 /* Length of message. */
 
-int main(void) {
-   struct my_msgbuf buf;
-   int msqid;
-   int toend;
-   key_t key;
-   
-   if ((key = ftok("test1totest2", 'B')) == -1) {
-      perror("ftok");
-      exit(1);
-   }
-   
-   if ((msqid = msgget(key, PERMS | IPC_CREAT)) == -1) { /* connect to the queue */
-      perror("msgget");
-      exit(1);
-   }
-   printf("message queue: ready to receive messages.\n");
-   
-   for(;;) { /* normally receiving never ends but just to make conclusion 
-             /* this program ends wuth string of end */
-      if (msgrcv(msqid, &buf, sizeof(buf.mtext), 0, 0) == -1) {
-         perror("msgrcv");
-         exit(1);
-      }
-      
-      printf("recvd: \"%s\"\n", buf.mtext);
-      toend = strcmp(buf.mtext,"end");
+/* The consumer is faster than the publisher. */
+#define QUEUE_POLL_CONSUMER ((struct timespec){1, 0})
 
-      if (toend == 0) 
-        break;
-   }
-   printf("message queue: done receiving messages.\n");
-//    system("rm msgq.txt");
-   return 0;
+int main() {
+	mqd_t mq;
+	struct timespec poll_sleep;
+	struct mq_attr attr;
+	unsigned int prio;
+	ssize_t bytes_read;
+	char buffer[QUEUE_MSGSIZE + 1];
+   
+	/* Create the message queue. The queue reader is NONBLOCK. */
+   attr.mq_flags = 0;
+   attr.mq_maxmsg = QUEUE_MAXMSG;
+   attr.mq_msgsize = QUEUE_MSGSIZE;
+   attr.mq_curmsgs = 0;
+   attr.__pad[0] = 0;
+   attr.__pad[1] = 0;
+   attr.__pad[2] = 0;
+   attr.__pad[3] = 0;
+	mq = mq_open(QUEUE_NAME, O_CREAT | O_RDONLY | O_NONBLOCK, QUEUE_PERMS, &attr);
+	if(mq < 0) {
+		fprintf(stderr, "[CONSUMER]: Error, cannot open the queue: %s.\n", strerror(errno));
+		exit(1);
+	}
+	
+	printf("[CONSUMER]: Queue opened, queue descriptor: %d.\n", mq);
+		
+	while(1) {
+		memset(buffer, 0x00, sizeof(buffer));
+		bytes_read = mq_receive(mq, buffer, QUEUE_MSGSIZE, &prio);
+		if(bytes_read >= 0) {
+			printf("[CONSUMER]: Received message: \"%s\"\n", buffer);
+		} else {
+			//printf("[CONSUMER]: No messages yet.\n");
+			poll_sleep = QUEUE_POLL_CONSUMER;
+			nanosleep(&poll_sleep, NULL);
+		}
+		
+		fflush(stdout);
+	}
+
+	printf("[CONSUMER]: Cleanup...\n");
+	mq_close(mq);
+	mq_unlink(QUEUE_NAME);
 }
